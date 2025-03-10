01fabb026ae8635e8764624b5bb8837f90fb4d27
davidv410 <davidvrgoc99@gmail.com>
2025-03-10 23:06:40 +0100

    plase work

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const cors = require('cors')
 const jwt = require('jsonwebtoken')
 const multer = require('multer')
